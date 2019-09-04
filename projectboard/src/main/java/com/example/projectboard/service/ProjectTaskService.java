9fb4d8209f11528c911d7f42ffa9d39d2f1171fb
prasanganath <sandun.wed@gmail.com>
2019-09-04 18:33:53 +0530

    initial commit

---
+++ projectboard/src/main/java/com/example/projectboard/service/ProjectTaskService.java
@@ -0,0 +1,21 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.domain.ProjectTask;
+import com.example.projectboard.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService  {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if (projectTask.getStatus()==null || projectTask.getStatus()==""){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
+}
---
+++ projectboard/src/main/java/com/example/projectboard/web/ProjectTaskController.java
@@ -0,0 +1,39 @@
+package com.example.projectboard.web;
+
+import com.example.projectboard.domain.ProjectTask;
+import com.example.projectboard.service.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/board")
+@CrossOrigin
+public class ProjectTaskController {
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @PostMapping("")
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask, BindingResult result){
+
+        if (result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        ProjectTask newPT = projectTaskService.saveOrUpdateProjectTask(projectTask);
+        return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
+    }
+
+}
