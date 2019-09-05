86f6353992d75e66161cd0acf9e5b0217820e6c8
prasanganath <sandun.wed@gmail.com>
2019-09-05 14:20:02 +0530

    update front end

--- project-board-react/src/App.js
+++ project-board-react/src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./components/Navbar";
 import ProjectBoard from "./components/ProjectBoard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProjectTask from "./components/ProjectTask/AddProjectTask";
 
 
 class App extends Component {
 
   render(){
     return (
+      <Router>
       <div className="App">
         <Navbar />
         <ProjectBoard />
-        
+        <Route exact path="/addProjectTask" component={AddProjectTask} />        
       </div>
+      </Router>
     );
   }
 }
